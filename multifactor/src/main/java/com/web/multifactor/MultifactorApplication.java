b617644a098111b92973f2450dbd3e0168100b27
plus12324 <sung-bm@hanmail.net>
2018-12-20 00:47:54 +0900

    init commit

---
+++ multifactor/src/main/java/com/web/multifactor/MultifactorApplication.java
@@ -0,0 +1,14 @@
+package com.web.multifactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultifactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultifactorApplication.class, args);
+	}
+
+}
+
