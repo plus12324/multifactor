8e72ef4510694ca3c2cb623d1188aaf9e8f4b125
educar <educar@172.24.208.1>
2019-01-24 18:35:23 +0900

    aa

--- multifactor/src/main/java/com/web/multifactor/service/ExcelBulkReadService.java
+++ multifactor/src/main/java/com/web/multifactor/service/ExcelBulkReadService.java
@@ -9,11 +9,20 @@ import java.util.List;
 import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.poi.openxml4j.opc.PackageAccess;
+import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.ooxml.util.SAXHelper;
+import org.apache.poi.xssf.eventusermodel.ReadOnlySharedStringsTable;
 import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.eventusermodel.XSSFReader.SheetIterator;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler.SheetContentsHandler;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
 import org.apache.poi.xssf.model.SharedStringsTable;
+import org.apache.poi.xssf.model.StylesTable;
+import org.apache.poi.xssf.usermodel.XSSFComment;
 import org.apache.poi.xssf.usermodel.XSSFRichTextString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -45,7 +54,35 @@ public class ExcelBulkReadService {
 	@Autowired
 	UserRepository userRepository;
 	
-	@Async
+//	@Async
+	public void processSheet(String filename) throws Exception {
+		try (OPCPackage opc = OPCPackage.open(filename, PackageAccess.READ)) {
+			XSSFReader r = new XSSFReader(opc);
+			XSSFReader.SheetIterator itr = (SheetIterator) r.getSheetsData();
+			
+			StylesTable styles = r.getStylesTable();
+			ReadOnlySharedStringsTable strings = new ReadOnlySharedStringsTable(opc);
+			
+			List<String[]> dataList = new ArrayList<String[]>();
+			
+			while(itr.hasNext()) {
+				try(InputStream sheetStream = itr.next();){
+					InputSource sheetSource =  new InputSource(sheetStream);
+					Sheet2ListHandler sheet2ListHandler = new Sheet2ListHandler(dataList, 10);
+					ContentHandler handler = new XSSFSheetXMLHandler(styles, strings, sheet2ListHandler, false);
+					
+					SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+					SAXParser saxParser = saxFactory.newSAXParser();
+					
+					XMLReader sheetParser = saxParser.getXMLReader();
+					sheetParser.setContentHandler(handler);
+					System.out.println("파싱시작");
+					sheetParser.parse(sheetSource);
+				}
+			}
+		}
+	}
+	
     public void processFirstSheet(String filename) throws Exception {
         try (OPCPackage pkg = OPCPackage.open(filename, PackageAccess.READ)) {
             XSSFReader r = new XSSFReader(pkg);
@@ -87,7 +124,95 @@ public class ExcelBulkReadService {
         parser.setContentHandler(handler);
         return parser;
     }
+    
+  
 
+    private class Sheet2ListHandler implements org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler.SheetContentsHandler{
+    	private boolean firstCellOfRow = false;
+    	private int currentCol = -1;
+    	
+    	private List<String[]> rows;
+    	private String[] row;
+    	private int columnCnt;
+    	private int currColNum = 0;
+    	
+    	public Sheet2ListHandler(List<String[]> rows, int columnsCnt) {
+    		this.rows = rows;
+    		this.columnCnt = columnsCnt;
+    	}
+    	
+//		@Override
+//		public void startRow(int rowNum) {
+//			// TODO Auto-generated method stub
+//			this.row = new String[columnCnt];
+//			currColNum = 0;
+//		}
+//		
+//		@Override
+//		public void endRow(int rowNum) {
+//			// TODO Auto-generated method stub
+//			boolean addFlag = false;
+//			for(String data : row) {
+//				if(!"".equals(data))
+//					addFlag = true;
+//			}
+//			
+//			if(addFlag) rows.add(row);
+//		}
+//		
+//		@Override
+//		public void cell(String cellReference, String formattedValue, XSSFComment comment) {
+//			// TODO Auto-generated method stub
+//			int curCol = (new CellReference(cellReference))	.getCol();
+//			int missedCols = curCol - currentCol - 1;
+//			for(int i=0; i < missedCols ; i++) {
+//				row[currColNum++]="";
+//			}
+//			currentCol = curCol;
+//			row[currColNum++] = formattedValue == null ? "" : formattedValue;
+//			System.out.println(row[currColNum++]);
+//		}
+		
+		@Override
+		public void headerFooter(String text, boolean isHeader, String tagName) {
+			// TODO Auto-generated method stub
+			SheetContentsHandler.super.headerFooter(text, isHeader, tagName);
+		}
+
+		@Override
+		public void startRow(int rowNum) {
+			System.out.println(rowNum);
+			// TODO Auto-generated method stub
+			this.row = new String[columnCnt];
+			currColNum = 0;
+		}
+		
+		@Override
+		public void endRow(int rowNum) {
+			System.out.println(rowNum);
+			// TODO Auto-generated method stub
+			boolean addFlag = false;
+			for(String data : row) {
+				if(!"".equals(data))
+					addFlag = true;
+			}
+			
+			if(addFlag) rows.add(row);
+		}
+		
+		@Override
+		public void cell(String cellReference, String formattedValue, XSSFComment comment) {
+			// TODO Auto-generated method stub
+			int curCol = (new CellReference(cellReference))	.getCol();
+			int missedCols = curCol - currentCol - 1;
+			for(int i=0; i < missedCols ; i++) {
+				row[currColNum++]="";
+			}
+			currentCol = curCol;
+			row[currColNum++] = formattedValue == null ? "" : formattedValue;
+			System.out.println(row[currColNum++]);
+		}
+    }
     /**
      * See org.xml.sax.helpers.DefaultHandler javadocs
      */
@@ -123,13 +248,13 @@ public class ExcelBulkReadService {
             // c => cell
             if(name.equals("c")) {
                 // Print the cell reference
-                System.out.print(attributes.getValue("r") + " - ");
+                
                 // Figure out if the value is an index in the SST
                 String cellType = attributes.getValue("t");
                 nextIsString = cellType != null && cellType.equals("s");
-                System.out.print(nextIsString + "/");
+                
                 inlineStr = cellType != null && cellType.equals("inlineStr");
-                System.out.print(inlineStr);
+                System.out.print(" |필드명 : " + attributes.getValue("r") +" / "+ nextIsString +" | ");
             }
             // Clear contents cache
             lastContents = "";
@@ -141,10 +266,10 @@ public class ExcelBulkReadService {
             // Process the last contents as required.
             // Do now, as characters() may be called more than once
             if(nextIsString) {
-            	System.out.println("■B : " + lastContents);
+            	
             	Integer idx = Integer.valueOf(lastContents);
                 lastContents = lruCache.get(idx);
-                System.out.println("■A : " + lastContents);
+                System.out.print("값 : " + lastContents);
                 
                 if (lastContents == null && !lruCache.containsKey(idx)) {
                     lastContents = new XSSFRichTextString(sst.getEntryAt(idx)).toString();
@@ -152,24 +277,23 @@ public class ExcelBulkReadService {
                 }
                 nextIsString = false;
             }
-
             // v => contents of a cell
             // Output after we've seen the string contents
             if(name.equals("v") || (inlineStr && name.equals("c"))) {
-                System.out.println(localName +" / "+ lastContents);
+//                System.out.println("");
             }
         }
 
         @Override
         public void characters(char[] ch, int start, int length) throws SAXException { // NOSONAR
             lastContents += new String(ch, start, length);
-            System.out.println("■ : " + lastContents);
+            if(!nextIsString) System.out.print("필드값 : " + lastContents);
         }
     }
 
     public static void main(String[] args) throws Exception {    	
     	ExcelBulkReadService howto = new ExcelBulkReadService();
-        howto.processFirstSheet("C:\\Users\\sungb_000\\git\\multifactor\\multifactor\\test.xlsx");
-//        howto.processAllSheets("C:\\Users\\sungb_000\\git\\multifactor\\multifactor\\test.xlsx");
+//        howto.processFirstSheet("C:\\Users\\educar\\git\\multifactor\\multifactor\\lll.xls");
+        howto.processSheet("C:\\Users\\educar\\git\\multifactor\\multifactor\\lll.xlsx");
     }
 }
